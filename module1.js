5fa72bf50d9d108f43f3c1d02dcf6c9a687c9e3b
nadilsandaruwan97@gmail.com <87482209+Nadil97@users.noreply.github.com>
2022-09-06 21:26:21 +0530

    Create module 1

---
+++ module1.js
@@ -0,0 +1,8 @@
+var companyTitle="ABC Software Solution Pvt.Ltd";
+var location="Colombo";
+var employees=["Scot","Allen","jones"];
+var companyTurnover=4.5;
+var getBranchDetailes=function(){
+    return["branch 1","branch 2"];
+};
+var director={directorName:"Nadil",experience:13};
