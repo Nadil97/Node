fb912008ac3cdeef246daa08b0a5b6ba4c274a77
nadilsandaruwan97@gmail.com <87482209+Nadil97@users.noreply.github.com>
2022-09-07 09:28:18 +0530

    modify

--- readFile.js
+++ readFile.js
@@ -1,8 +1,12 @@
 var fs =require("fs");
-fs.readFile("sample.txt",receiveFileContent);
+fs.readFile("sample.txt","binary",receiveFileContent);
 
 function receiveFileContent(error,data)
 {
+if(error){
     console.log(error);
+}
+else{
     console.log(data);
+}
 }
